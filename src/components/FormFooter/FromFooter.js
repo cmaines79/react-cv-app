a939930213410df8ae1d9fb43a32866fa538aee2
cmaines79 <cmaines79@gmail.com>
2021-10-18 09:48:07 -0600

    charlie2
    asd

--- src/components/FormFooter/FromFooter.js
+++ src/components/FormFooter/FromFooter.js
@@ -1,13 +1,21 @@
 import './FormFooter.css';
 
 const FromFooter = ({ sampleData }) => {
+<<<<<<< Updated upstream
     const generatePDF = () => {
+=======
+    generatePDF = () => {
+>>>>>>> Stashed changes
         alert('This feature is coming soon!');
     }
 
     return (
         <div className="form-footer">
+<<<<<<< Updated upstream
             <button className="btn btn-green" onClick={generatePDF}>Generate PDF</button>
+=======
+            <button className="btn btn-green" onClick={generatePDF()}>Generate PDF</button>
+>>>>>>> Stashed changes
             <button className="btn btn-orange" onClick={sampleData}>Load Example</button>
             <button className="btn btn-red">Reset</button>
         </div>
