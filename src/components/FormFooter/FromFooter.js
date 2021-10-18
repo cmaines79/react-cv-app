f92e464c34cdf19d30657d03b41844073f387c9e
cmaines79 <cmaines79@gmail.com>
2021-10-18 09:47:40 -0600

    Charlie
    Updated the generate PDF button to an alert that says 'this feature is coming soon'.
    
    I still can't figure out how to load the headshot image from the state variable....

--- src/components/FormFooter/FromFooter.js
+++ src/components/FormFooter/FromFooter.js
@@ -1,9 +1,13 @@
 import './FormFooter.css';
 
 const FromFooter = ({ sampleData }) => {
+    const generatePDF = () => {
+        alert('This feature is coming soon!');
+    }
+
     return (
         <div className="form-footer">
-            <button className="btn btn-green">Generate PDF</button>
+            <button className="btn btn-green" onClick={generatePDF}>Generate PDF</button>
             <button className="btn btn-orange" onClick={sampleData}>Load Example</button>
             <button className="btn btn-red">Reset</button>
         </div>
